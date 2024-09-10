57c3374009510816a9b5221f8507b2380abecf6e
Donhadley <donhadleygirlandchika@gmail.com>
2024-09-10 08:05:11 +0100

    reconfigured terraform and CI/CD files

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,14 +89,83 @@ resource "aws_key_pair" "my_key_pair" {
   public_key = file(var.public_key_path)  # Using the path to the public key file
 }
 
+# Create an ECR repository
+resource "aws_ecr_repository" "app_repo" {
+  name                 = "my-app-repo"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 
-# Create EC2 Instance
-resource "aws_instance" "app_instance" {
-  ami               = "ami-0a5c3558529277641"  # Amazon Linux 2 AMI ID (update based on your region)
-  instance_type     = "t2.micro"
-  subnet_id         = aws_subnet.public_subnet.id
-  key_name          = aws_key_pair.my_key_pair.key_name
-  vpc_security_group_ids = [aws_security_group.app_sg.id]  # Use Security Group ID
+  tags = {
+    Name = "AppECRRepository"
+  }
+}
+
+# IAM Role for ECR access (for EC2 or CI/CD)
+resource "aws_iam_role" "ecr_access_role" {
+  name = "EC2ECRAccessRole"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+}
+
+# Attach policy to allow EC2 to interact with ECR
+resource "aws_iam_role_policy" "ecr_access_policy" {
+  role = aws_iam_role.ecr_access_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:PutImage",
+        "ecr:InitiateLayerUpload",
+        "ecr:UploadLayerPart",
+        "ecr:CompleteLayerUpload"
+      ],
+      "Resource": "${aws_ecr_repository.app_repo.arn}"
+    },
+    {
+      "Effect": "Allow",
+      "Action": "ecr:ListImages",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# Attach the IAM role to EC2 instance
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "EC2InstanceProfile"
+  role = aws_iam_role.ecr_access_role.name
+}
+
+# Create EC2 Instance with IAM Role
+resource "aws_instance" "app_instance_with_profile" {
+  ami                         = "ami-0a5c3558529277641"  # Amazon Linux 2 AMI ID (update based on your region)
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids       = [aws_security_group.app_sg.id]
+  key_name                    = aws_key_pair.my_key_pair.key_name
+  iam_instance_profile        = aws_iam_instance_profile.instance_profile.name
 
   tags = {
     Name = "NodeJS-EC2"
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,30 @@
+# Output the ECR repository URL
+output "ecr_repository_url" {
+  description = "The URL of the ECR repository"
+  value       = aws_ecr_repository.app_repo.repository_url
+}
+
+# Output the EC2 instance public IP
+output "ec2_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.app_instance_with_profile.public_ip
+}
+
+# Output the VPC ID
 output "vpc_id" {
-  description = "The ID of the VPC"
+  description = "The ID of the created VPC"
   value       = aws_vpc.custom_vpc.id
 }
 
-output "public_subnet_id" {
-  description = "The ID of the public subnet"
-  value       = aws_subnet.public_subnet.id
+# Output the EC2 Instance ID
+output "ec2_instance_id" {
+  description = "The ID of the created EC2 instance"
+  value       = aws_instance.app_instance_with_profile.id
 }
 
-output "ec2_public_ip" {
-  description = "The public IP of the EC2 instance"
-  value       = aws_instance.app_instance.public_ip
+# Output the IAM role used by the EC2 instance
+output "ec2_iam_role" {
+  description = "The IAM role associated with the EC2 instance"
+  value       = aws_iam_role.ecr_access_role.name
 }
+
